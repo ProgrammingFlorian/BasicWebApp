8f4ab6b5c852bf0fe453707c387f6fc99ba1c4ab
Florian <ge95kek@mytum.de>
2022-07-14 15:11:01 +0200

    challenge

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,15 +6,39 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "F";
+        } else if (query.contains("which of the following numbers is the largest")) {
+            String numbers = query.substring("which of the following numbers is the largest: ".length());
+            String[] n = numbers.split(", ");
+            int largest = Integer.MIN_VALUE;
+            for (int i = 0; i < n.length; i++) {
+                try {
+                    int num = Integer.parseInt(n[i]);
+                    if(num > largest) {
+                        largest = num;
+                    }
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "" + largest;
+        } else if (query.contains("what is ")) {
+            String[] parts = query.split(" ");
+            int result = 0;
+            try {
+                int a = Integer.parseInt(parts[parts.length - 1]);
+                int b = Integer.parseInt(parts[parts.length - 3]);
+                result = a + b;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            return "" + result;
         } else { // TODO extend the programm here
-            return "";
+            return "F";
         }
     }
 }
